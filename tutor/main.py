701e65788304666ac9620912ce6e840e66ec1cb5
ZhuZiLiBryan <bryanzhuzili@gmail.com>
2024-07-12 00:44:20 -0700

    use os library to automate path to audio

--- tutor/main.py
+++ tutor/main.py
@@ -1,8 +1,9 @@
 import sounddevice as sd
 import whisper 
 import torch
+import os
 
-
+# Initialize CUDA capabilities from PyTorch and query device properties
 torch.cuda.init()
 num_devices = torch.cuda.device_count()
 device_name = torch.cuda.get_device_name()
@@ -10,6 +11,10 @@ compute_capability = torch.cuda.get_device_capability()
 print(f"Number of Devices: {num_devices}")
 print(f"Device Name: {device_name}")
 
+# Load Whisper model
 model = whisper.load_model("base")
-result = model.transcribe("C:/Users/Bryan/Documents/GitHub/JapaneseConversationTutor/tutor/testaudio.mp3")
+
+# Find audio file to transcribe
+abs_path = os.path.abspath("tutor/audio/testaudio.mp3")
+result = model.transcribe(abs_path)
 print(result["text"])
