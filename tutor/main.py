5b94cfcdb08059d651011927fdc724c672750671
ZhuZiLiBryan <bryanzhuzili@gmail.com>
2024-07-12 00:05:08 -0700

    updated requirements.txt for ffmpeg and ran first succesful audio -> text transcription

--- tutor/main.py
+++ tutor/main.py
@@ -2,10 +2,9 @@ import sounddevice as sd
 import whisper 
 
 model = whisper.load_model("base")
-result = model.transcribe("testaudio.mp3")
+result = model.transcribe("C:/Users/Bryan/Documents/GitHub/JapaneseConversationTutor/tutor/testaudio.mp3")
 print(result["text"])
 
-#TODO: Install ffmpeg
 #TODO: Install PyTorch with CUDA support
 
 print("Test")
