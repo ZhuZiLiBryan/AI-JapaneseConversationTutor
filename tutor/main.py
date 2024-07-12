1954bbe0e48b51dff7c66762846a85691a58a6ef
ZhuZiLiBryan <bryanzhuzili@gmail.com>
2024-07-12 00:30:11 -0700

    Cuda initializer and device query

--- tutor/main.py
+++ tutor/main.py
@@ -2,9 +2,14 @@ import sounddevice as sd
 import whisper 
 import torch
 
+
+torch.cuda.init()
+num_devices = torch.cuda.device_count()
+device_name = torch.cuda.get_device_name()
+compute_capability = torch.cuda.get_device_capability()
+print(f"Number of Devices: {num_devices}")
+print(f"Device Name: {device_name}")
+
 model = whisper.load_model("base")
 result = model.transcribe("C:/Users/Bryan/Documents/GitHub/JapaneseConversationTutor/tutor/testaudio.mp3")
 print(result["text"])
-
-
-print("Test")
