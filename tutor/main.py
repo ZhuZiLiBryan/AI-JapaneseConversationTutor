f8f1ffcaa62ab104dcfaa298cebf5ee40e6dfb45
ZhuZiLiBryan <bryanzhuzili@gmail.com>
2024-07-14 01:32:15 -0700

    succesful transcriptions with recognizing microphone audio

--- tutor/main.py
+++ tutor/main.py
@@ -6,8 +6,8 @@ import pyaudio
 import speech_recognition as sr
 
 # TODO: Use Django? for web frontend
-# TODO: 
-# 
+# TODO: Use microphone stream for real-time transcription
+
 
 # Initialize CUDA capabilities from PyTorch and query device properties
 torch.cuda.init()
@@ -26,20 +26,31 @@ print(f"GPU Name: {device_name}")
     finally:d
         print("Program Terminating! See you again soon!")'''
 
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Test audio!:" )
-    audio = r.listen(source)
-
-with open("tutor/audio/recording_results.wav", "wb") as f:
-    f.write(audio.get_wav_data())
-
-
-
 # Load Whisper model
 model = whisper.load_model("base")
 
 # Find audio file to transcribe
 abs_path = os.path.abspath("tutor/audio/recording_results.wav")
-result = model.transcribe(abs_path)
-print(result["text"])
+
+# TODO: in frontend allow tweaking of automatic energy_threshold and sliding bar for manual calibration (see speech recog documentation)
+r = sr.Recognizer()
+
+while True:
+    with sr.Microphone() as source:
+        print("Test audio!:" )
+        audio = r.listen(source)
+
+    with open("tutor/audio/recording_results.wav", "wb") as f:
+        f.write(audio.get_wav_data())
+
+    result = model.transcribe(abs_path)
+    print(f'{result["text"]}, {len(result["text"])}') 
+
+    # Microphone picks up random noise, no transcription
+    if not result["text"]:
+        continue
+
+    #TODO: do OpenAI API call only if audio detected was valid (i.e. length of transcribed audio is nonzero)
+
+
+
