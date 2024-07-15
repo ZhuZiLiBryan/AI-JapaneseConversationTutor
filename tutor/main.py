d0464b5298a21b6f7794e708c2b8b6199f1e4d0d
ZhuZiLiBryan <bryanzhuzili@gmail.com>
2024-07-15 01:32:32 -0700

    use gTTS for text to speech testing

--- tutor/main.py
+++ tutor/main.py
@@ -4,6 +4,7 @@ import torch
 import os
 import pyaudio
 import speech_recognition as sr
+from gtts import gTTS
 
 # TODO: Use Django? for web frontend
 # TODO: Use microphone stream for real-time transcription
@@ -16,16 +17,6 @@ device_name = torch.cuda.get_device_name()
 compute_capability = torch.cuda.get_device_capability()
 print(f"GPU Name: {device_name}")
 
-# main loop
-
-'''while True:
-    try:
-      continue  
-    except KeyboardInterrupt:
-        print("CTRL-C pressed")
-    finally:d
-        print("Program Terminating! See you again soon!")'''
-
 # Load Whisper model
 model = whisper.load_model("base")
 
@@ -35,22 +26,31 @@ abs_path = os.path.abspath("tutor/audio/recording_results.wav")
 # TODO: in frontend allow tweaking of automatic energy_threshold and sliding bar for manual calibration (see speech recog documentation)
 r = sr.Recognizer()
 
+# control what language gTTS uses
+selected_language = "ja";
+
+# Main loop
 while True:
+    # Listen for microphone audio
     with sr.Microphone() as source:
         print("Test audio!:" )
         audio = r.listen(source)
 
+    # Write detected audio to wav formatted file
     with open("tutor/audio/recording_results.wav", "wb") as f:
         f.write(audio.get_wav_data())
 
+    # Transcribe audio
     result = model.transcribe(abs_path)
-    print(f'{result["text"]}, {len(result["text"])}') 
 
     # Microphone picks up random noise, no transcription
     if not result["text"]:
         continue
 
+    print(f'{result["text"]}, {len(result["text"])}') 
     #TODO: do OpenAI API call only if audio detected was valid (i.e. length of transcribed audio is nonzero)
+    tts = gTTS(result["text"], lang=selected_language)
+    tts.save("testplayback.mp3")
 
 
 
