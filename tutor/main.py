98c49f3da3b480f370e624ce8c553e76d5b570c6
ZhuZiLiBryan <bryanzhuzili@gmail.com>
2024-07-23 21:48:00 -0700

    linked openai api

--- tutor/main.py
+++ tutor/main.py
@@ -6,6 +6,8 @@ import pyaudio
 import speech_recognition as sr
 from gtts import gTTS
 import playsound
+from dotenv import dotenv_values
+from openai import OpenAI
 
 # TODO: Use Django? for web frontend
 # TODO: Use microphone stream for real-time transcription
@@ -30,6 +32,30 @@ r = sr.Recognizer()
 # control what language gTTS uses
 selected_language = "ja";
 
+
+# Get OpenAI API Key
+config = dotenv_values('.env')
+
+# Establish OpenAI API client
+client = OpenAI (
+    api_key = config['OPENAI_API_KEY']
+)
+
+# initial_prompt to be used to define AI's role
+initial_prompt = """
+    You are a friendly Japanese tutor named 井芹 仁菜.  You enjoy listening to rock music, watching anime, and playing video games.
+    You speak only in Japanese, although when the user is unsure of how to say something in Japanese, you help them come up with the right word.
+    You are to chat with the user like a tutor/friend in a conversational manner.
+"""
+# list of messages to be updated as conversation continues    
+chat_history = [
+    {
+        "role": "system", 
+        "content": initial_prompt
+    }
+]
+
+
 # Main loop
 while True:
     # Listen for microphone audio
@@ -49,11 +75,33 @@ while True:
         continue
 
     print(f'{result["text"]}, {len(result["text"])}') 
-    #TODO: do OpenAI API call only if audio detected was valid (i.e. length of transcribed audio is nonzero)
+    # append new user messages to chat history
+    chat_history.append(
+        {
+            "role": "user", 
+            "content": result["text"]
+        }
+    )
+
+    # do OpenAI API call only if audio detected was valid (i.e. length of transcribed audio is nonzero)
+    # and extract response 
+    response = client.chat.completions.create(
+        messages=chat_history,
+        model="gpt-4o-mini",
+    )
+    tutor_response = response.choices[0].message.content
+
+    # append OpenAI generated messages
+    chat_history.append(
+        {
+            "role": "assistant",
+            "content": tutor_response
+        }
+    )
 
     # For now, use Google TTS for TTS
     #TODO: use OpenAI TTS for natural transcription
-    tts = gTTS(result["text"], lang=selected_language)
+    tts = gTTS(tutor_response, lang=selected_language)
     tts.save("testplayback.mp3")
 
     # Play audio
